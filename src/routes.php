33b17cbc3a56b58c15c2643d380fd529bfd388db
Pietro Rignanese <pietro_rignanese@hotmail.it>
2018-11-29 15:02:44 +0100

    Add user/post/credentials

--- src/routes.php
+++ src/routes.php
@@ -21,6 +21,19 @@ $app->group('/user', function () {
     });
 //______________________________________________
     
+//_____________POST__________________________
+     $this->group('/post', function () {
+        $this->post('/credentials', function(Request $request, Response $response){
+           $result = $this->db->query("INSERT INTO users(id, mail) VALUES ('$this->token_id','$this->token_mail')");
+            if(!$result){
+                return $response->withJson(false,422);
+                } else {
+                return $response->withJson($result ,200);
+            }
+        });
+    });
+//____________________________________________
+    
 //__________DELETE____________________________
     $this->group('/delete', function () {
 
