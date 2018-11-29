54c460d2d3a5f31eaee68e196746733b51708d4e
Pietro Rignanese <pietro_rignanese@hotmail.it>
2018-11-29 14:29:32 +0100

    Add configuration DB
    Configurazione del DB

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreapolenta
+ * Date: 20/11/2018
+ * Time: 11:47
+ */
+
+// Local Data
+$host = 'localhost:3307';
+$dbname = 'hack2018';
+$user = 'root';
+$pass = 'root';
+
+// Server Data
+//$host = 'http://90.147.188.51/RES/backend/public/';
+//$dbname = 'res_db';
+//$user = 'hu01';
+//$pass = '60Ld0N6k';
