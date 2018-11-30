c03d6eb2e8d105bf0c652a66c596e5106a123399
Gwynbleidd <andrea.polenta@outlook.it>
2018-11-30 01:05:04 +0100

    Add Cors

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
@@ -17,6 +17,18 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
@@ -27,4 +39,9 @@ require __DIR__ . '/../src/middleware.php';
 require __DIR__ . '/../src/routes.php';
 
 // Run app
-$app->run();
+try {
+    $app->run();
+} catch (\Slim\Exception\MethodNotAllowedException $e) {
+} catch (\Slim\Exception\NotFoundException $e) {
+} catch (Exception $e) {
+}
